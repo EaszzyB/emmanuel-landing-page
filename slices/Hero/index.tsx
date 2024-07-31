01c26579de0de28ef464bf47debc1d60202479d3
Emmanuel <Emmanuelafolabi2026@gmail.com>
2024-07-31 01:21:45 -0400

    Name animation

--- slices/Hero/index.tsx
+++ slices/Hero/index.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { useEffect, useRef } from "react";
 
 import { Content, KeyTextField } from "@prismicio/client";
@@ -14,11 +16,45 @@ export type HeroProps = SliceComponentProps<Content.HeroSlice>;
  * Component for "Hero" Slices.
  */
 const Hero = ({ slice }: HeroProps): JSX.Element => { 
-cost components = useRef(null);
+const components = useRef(null);
 
 useEffect(() => {
   let ctx = gsap.context(()=> {
+      const tl = gsap.timeline();
 
+      tl.fromTo(
+        ".name-animation", {
+        x: -100, 
+        opacity:0, 
+        rotate: -10
+      },
+      {
+        x:0,
+        opacity:1,
+        rotate:0,
+        ease: "elastic.out(1,0.3)",
+        duration: 1,
+        transformOrigin: "left top",
+        stagger: {
+          each: 0.1,
+          from: "random",
+        }
+      }
+    )
+
+    tl.fromTo(
+      ".job-title", {
+        y:20,
+        opacity:0,
+        scale: 1.2
+      }, {
+        opacity:1,
+        y:0,
+        duration: 1,
+        scale: 1,
+        ease: "elastic.out(1,0.3)",
+      }
+    )
 
   }, components);
   return () => ctx.revert();
@@ -40,10 +76,11 @@ const renderLettters = (name:KeyTextField, key:string) => {
     <section
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
+      ref={components}
     >
       <div className="grid min-h-[70vh] grid-cold-1 md:grid-cols-2 items-center">
         <div className="col-start-1 mid:row-start-1">
-          <h1 className="mb-8 text-[clamp(3rem,20vmin,20rem)] font-extrabold leading-none
+          <h1 className="mb-8 text-[clamp(3rem,18vmin,18rem)] font-extrabold leading-none
           tracking-tighter" aria-label={slice.primary.first_name + " " + slice.primary.last_name}>
       <span className="block text-slate-300">
         {renderLettters(slice.primary.first_name, "first")}
@@ -52,9 +89,9 @@ const renderLettters = (name:KeyTextField, key:string) => {
         {renderLettters(slice.primary.last_name, "last")}
         </span>
         </h1>
-        <span className="block bg-gradient-to-tr from-yellow-500 via-blue-700
+        <span className="job-title block bg-gradient-to-tr from-yellow-500 via-orange-700
         to-yellow-500 bg-clip-text text-2xl font-bold uppercase tracking-[.2em] 
-        text-transparent opacity-100 md:text-4xl">
+        text-transparent opacity-0 md:text-4xl">
           {slice.primary.tag_line}
           </span>
        </div>
